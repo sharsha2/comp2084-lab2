a7cb5d243501c48681bab857ccfbfd2fab31db7c
Sri Satya Harsha Karuturi <karuturiharsh@gmail.com>
2020-05-29 01:39:19 -0400

    i've added the About view project amd connected to the Nav Bar.!

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace LAb2.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
