d0a25aa3c6730dfc9d8b02046c047c1319f95777
Sri Satya Harsha Karuturi <karuturiharsh@gmail.com>
2020-05-29 01:29:57 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LAb2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
